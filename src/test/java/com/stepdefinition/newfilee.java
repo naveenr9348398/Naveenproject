73391bf761398d56e381d5b30e007b93e4e0cb10
naveenr9348398 <naveen.rajasekaran74@gmail.com>
2024-10-23 16:06:29 +0530

    updatedfile

---
+++ src/test/java/com/stepdefinition/newfilee.java
@@ -0,0 +1,12 @@
+package com.stepdefinition;
+
+
+public class newfilee {
+	
+	public static void main(String[] args) {
+         System.out.println("output1");	
+         System.out.println("output2");
+         System.out.println("output3");
+	}
+
+}
